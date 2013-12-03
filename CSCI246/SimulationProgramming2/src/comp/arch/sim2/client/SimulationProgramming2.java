d4e0e28c32b1bd6240a9c71c317ec6c414859048
James Cha <james.cha.zero@gmail.com>
2013-12-02 22:09:56 -0800

    added new School Project

---
+++ CSCI246/SimulationProgramming2/src/comp/arch/sim2/client/SimulationProgramming2.java
@@ -0,0 +1,65 @@
+package comp.arch.sim2.client;
+
+import comp.arch.sim2.shared.FieldVerifier;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.event.dom.client.KeyUpEvent;
+import com.google.gwt.event.dom.client.KeyUpHandler;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class SimulationProgramming2 implements EntryPoint {
+	/**
+	 * The message displayed to the user when the server cannot be reached or
+	 * returns an error.
+	 */
+	private static final String SERVER_ERROR = "An error occurred while "
+			+ "attempting to contact the server. Please check your network "
+			+ "connection and try again.";
+
+	/**
+	 * Create a remote service proxy to talk to the server-side Greeting service.
+	 */
+	private final GreetingServiceAsync greetingService = GWT
+			.create(GreetingService.class);
+
+	/**
+	 * This is the entry point method.
+	 */
+	public void onModuleLoad() {
+		Button instrExecButton = new Button();
+		Button displayContentsButton = new Button();
+		Button summerizeButton = new Button();
+		
+		ListBox loadStoreList = new ListBox();
+		ListBox smpNodesList = new ListBox();
+		
+		HorizontalPanel instrExecPanel = new HorizontalPanel();
+		HorizontalPanel displayContentsPanel = new HorizontalPanel();
+		HorizontalPanel summerizePanel = new HorizontalPanel();
+		
+		instrExecPanel.add(loadStoreList);
+		instrExecPanel.add(instrExecButton);
+		
+		displayContentsPanel.add(smpNodesList);
+		displayContentsPanel.add(displayContentsButton);
+		
+		summerizePanel.add(summerizeButton);
+		
+	}
+}
